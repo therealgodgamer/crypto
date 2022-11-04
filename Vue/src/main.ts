39fdbeaeab3d6f06d019eb2fbbb384b7b1713e83
dchi0021 <dchi0021@student.monash.edu>
2022-11-04 22:07:47 +0800

    Remove typescript

--- Vue/src/main.ts
+++ Vue/src/main.ts
@@ -2,9 +2,8 @@ import { createApp, VueElement } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import Paginate from "vuejs-paginate-next";
 const app = createApp(App);
 
-app.use(router).use(Paginate);
+app.use(router);
 
 app.mount("#app");
